6440fbfb608a0792e4a644291703c62f03069c6c
sasaki-k <siroken3@gmail.com>
2012-10-16 01:23:29 +0900

    handleNotificationを除いたClientの実装

--- client.py
+++ client.py
@@ -22,7 +22,7 @@ class Client:
         self.__myAddress =  (myIpAddress, port)
 
         for host in startupHostsList:
-            member = Member(host, 0, self, self.t_cleanup)
+            member = Member(host, 0, self, self.__t_cleanup)
             if host.count(myIpAddress) > 0:
                 self.__me = member
                 port = host[1]
@@ -40,6 +40,9 @@ class Client:
         else:
             print "Cloud not find myself in startup list"
             sys.exit(-1)
+
+        self.__deadListCond = threading.Condition()
+        self.__memberListCond = threading.Condition()
 
     def sendMembershipList(self):
         self.__me.heartbeat = self.__me.heartbeat + 1
@@ -68,8 +71,8 @@ class Client:
 
         def run(self):
             while self.__keepRunning:
-                time.sleepTime(self.__t_gossip)
-                self.sendMembershipList()
+                time.sleepTime(Client.self.__t_gossip)
+                Client.self.sendMembershipList()
 
     class AsynchronousReceiver(threading.Thread):
         def __init__(self):
@@ -78,16 +81,55 @@ class Client:
 
         def run(self):
             while self.__keepRunning:
-                buf, addr = self.__server.recvfrom(2048)
+                buf, addr = Client.self.__server.recvfrom(2048)
                 remotelist = pickle.loads(buf)
                 self.mergeLists(remotelist)
 
         def mergeLists(self, remoteList):
-            pass
+            with Client.self.__deadListCond:
+                with Client.self.__memberListCond:
+                    for remoteMember in remotelist:
+                        if remoteMember in Client.self.__memberList:
+                            localMember = Client.self.__memberList[Client.self.__memberList.index(remoteMember)]
+                            if remoteMember.heartbeat > localMember.heartbeat:
+                                // update local list with latest heatbeat
+                                localmember.heartbeat = remoteMember.heartbeat
+                                // and reset the timeout of that member
+                                localMember.resetTimeoutTimer()
+                        else:
+                            // the local list does not contain the remote member
+
+                            // the remote member is either brand new, or a previously declared dead member
+                            // if its dead, check the heatbeat because it may have come back from the dead
+                            if remoteMember in Client.self.__deadList:
+                                localDeadMember = Client.self.__deadList[Client.self.__deadList.index(remoteMember)]
+                                if remoteMember.heartbeat > localDeadMember.heartbeat:
+                                    // its baa-aack
+                                    del Client.self.__deadList[Client.self.__deadList.index(remoteMember)]
+                                    newLocalMember = Member(remoteMember.address, remoteMember.heartbeat, Client.self, Client.self.__t_cleanup)
+                                    Client.self.__memberList.append(newLocalMember)
+                                    newLocalMember.startTimeoutTimer()
+                                else:
+                                    pass // else ignore
+                            else:
+                                // brand spanking new member - welcome
+                                newLocalMember = Member(remoteMember.address, remoteMember.heartbeat, Client.self, Client.self.__t_cleanup)
+                                Client.self.__memberList.append(newLocalMember)
+                                newLocalMember.startTimeoutTimer()
 
     def start(self):
+        // Start all timers except for me
+        for member in self.__memberList:
+            if member != self.__me:
+                member.startTimeoutTimer()
+
         gossiper = self.MembershipGosspier()
+        gossiper.start()
         reciver = self.AsynchronousReceiver()
+        reciver.start()
+
+        while True:
+            time.sleepTime(10)
 
     def handleNotification(self):
         pass
